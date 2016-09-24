552fbb69a082c7bd8342c8f049d242ecbab5147f
Khoi Le <khoileee89@gmail.com>
2016-09-23 21:46:18 -0400

    initial formView page

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 // Call report  button that jumps to Report a problem form
+// septic system malfunction
+// not just test the water but the efficency and performance of home water treamtment equpiment
+
+//Form View
+//Color Index in which reporters can choose the "color" of the water present
 //
+
+
+$("#sight").css("background-image","../images/eye.png")
