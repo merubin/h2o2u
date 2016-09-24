496f0835e9ada6eb468f0069b146366e77dcaa8a
Khoi Le <khoileee89@gmail.com>
2016-09-24 14:16:03 -0400

    MVP

--- h2o2u-service/src/web/views/Taste.js
+++ h2o2u-service/src/web/views/Taste.js
@@ -21,7 +21,7 @@ export default class Col extends React.Component {
           </div >
 
              <div style={{background: 'white'}}>
-                 <span className="tasteSpan" style={{color:"#00488E;"}}>Do not taste the water if you haven't already.</span>
+                 <div className="tasteSpan" style={{color:"#00488E;"}}>Do not taste the water if you havent already.</div>
 
                  <List className="tasteList">
                     <ListItem primaryText="Sweet" leftCheckbox={<Checkbox />} />
