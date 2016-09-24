9dda485f5bdf3d7e75975bdc802136c559344b03
Michael Rubin <michael@rubinsworld.com>
2016-09-24 12:12:31 -0400

    color fixed

---
+++ h2o2u-service/src/web/views/Col.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default class Col extends React.Component {
+
+    render() {
+      console.log("Color is here");
+        return (
+         <div>
+          <h1>Color</h1>
+          <div id="eyeimg">
+            <img id="eye" src="http://rubinsworld.com/h2o2u/eye2.gif" alt="eye"/>
+          </div>
+          </div>
+    )
+    }
+}
--- h2o2u-service/src/web/views/app.js
+++ h2o2u-service/src/web/views/app.js
@@ -4,15 +4,17 @@ import User from 'User'
 import PageNotFound from 'PageNotFound'
 import MainApp from 'MainApp'
 import Yes from 'Yes'
+import Col from 'Col'
 import { browserHistory, Router, Route, Link } from 'react-router';
 import 'h202.css';
+import "Col.css";
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 render((
     <MuiThemeProvider>
         <Router>
             <Route path="/yes" component={Yes} />
-
+            <Route path="/color" component={Col} />
             <Route path="/" component={MainApp}>
                 <Route path="user/:userID" component={User} />
             </Route>
